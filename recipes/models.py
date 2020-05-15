b4b85c07c64600735f677c86e4800ab6ef76db60
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2020-05-15 13:50:59 +0100

    add Recipe and RecipeIngredients models to recipes/models.py

--- recipes/models.py
+++ recipes/models.py
@@ -6,3 +6,18 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    ingredients = models.ManyToManyField(Ingredient, through="RecipeIngredients")
+    date_created = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class RecipeIngredients(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField()
